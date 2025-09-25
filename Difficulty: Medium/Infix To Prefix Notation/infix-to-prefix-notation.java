158bde78c349540a4fb12e66125224a9b4d9c5bf
Akash Pratap Singh <149014095+akashpratapsing@users.noreply.github.com>
2025-09-25 16:14:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Infix To Prefix Notation/infix-to-prefix-notation.java
@@ -0,0 +1,55 @@
+class Solution {
+    public String infixToPrefix(String s) {
+        // code here
+        Stack<Character> st = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        s = new StringBuilder(s).reverse().toString();
+        
+        for (char ch : s.toCharArray()){
+            
+            if (Character.isLetterOrDigit(ch)){
+                sb.append(ch);
+            }else if (ch == ')'){
+                st.push(ch);
+            }else if (ch == '('){
+                while (!st.isEmpty() && st.peek() != ')'){
+                    sb.append(st.pop());
+                }
+                if (!st.isEmpty()) st.pop();
+            }else {
+                while (!st.isEmpty() && 
+                (
+                    (ch != '^' &&  priority(ch) < priority(st.peek())) ||
+                    (ch == '^' && priority(ch) <= priority(st.peek()))
+                    )){
+                    sb.append(st.pop());
+                }
+                st.push(ch);
+            }
+        }
+        while (!st.isEmpty()){
+            sb.append(st.pop());
+        }
+        
+        return String.valueOf(sb.reverse());
+        
+    }
+    
+    public static int priority(char ch){
+        switch(ch){
+            
+            case '+':
+            case '-':
+                return 1;
+                
+            case '*':
+            case '/':
+                return 2;
+                
+            case '^':
+                return 3;
+    
+        }
+        return -1;
+    }
+}
