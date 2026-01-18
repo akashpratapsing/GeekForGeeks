d7c050a46db9f2fae3be5eb8625fcb4fde89883f
Akash Pratap Singh <149014095+akashpratapsing@users.noreply.github.com>
2026-01-18 12:52:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Minimum Cost to Cut a Stick of length N/minimum-cost-to-cut-a-stick-of-length-n.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minCutCost(int n, int[] cuts) {
+        // code here
+        int m = cuts.length;
+        int[] cutsOrder = new int[m + 2];  
+        cutsOrder[0] = 0;
+        cutsOrder[m + 1] = n;
+
+        for (int i = 0; i < m; i++) {
+            cutsOrder[i + 1] = cuts[i];
+        }
+
+        Arrays.sort(cutsOrder);
+        int[][] dp = new int[m + 2][m + 2];
+
+        for (int i = m; i >= 1; i--) {
+            for (int j = i; j <= m; j++) {
+                int min = Integer.MAX_VALUE;
+                for (int k = i; k <= j; k++) {
+                    int cost = cutsOrder[j + 1] - cutsOrder[i - 1] + dp[i][k - 1] + dp[k + 1][j];
+                    min = Math.min(min, cost);
+                }
+                dp[i][j] = min;
+            }
+        }
+        return dp[1][m];
+    }
+}
